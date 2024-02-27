191a912e14c4ed41f29ccec0847e717f21e12025
kalyibek <kkalyibek@gmail.com>
2024-02-27 20:27:15 +0600

    updated views and urls

--- ww/urls.py
+++ ww/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.students_list, name='student_list'),
-    path('<int:pk>/', views.student_detail, name='student_detail'),
-    path('create/', views.student_create, name='student_create'),
-    path('delete/<int:pk>/', views.student_delete, name='student_delete'),
-    path('update/<int:pk>/', views.student_update, name='student_update')
+    path('', views.StudentsListView.as_view(), name='student_list'),
+    path('<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
+    path('create/', views.StudentCreateView.as_view(), name='student_create'),
+    path('delete/<int:pk>/', views.StudentDeleteView.as_view(), name='student_delete'),
+    path('update/<int:pk>/', views.StudentUpdateView.as_view(), name='student_update')
 ]
--- ww/views.py
+++ ww/views.py
@@ -1,28 +1,46 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 from . import models
 
 
-def students_list(request):
-    students = models.Student.objects.all()
-    return render(
-        request,
-        'ww/students.html',
-        {'students': students}
-    )
+class StudentsListView(View):
+    students: tuple
+
+    def get(self, request):
+        self.students = models.Student.objects.all()
+        return render(
+            request,
+            'ww/students.html',
+            {'students': self.students}
+        )
 
 
-def student_detail(request, pk):
-    student = get_object_or_404(models.Student, pk=pk)
-    return render(
-        request,
-        'ww/student.html',
-        {'student': student}
-    )
+class StudentDetailView(View):
+    student: models.Student
+
+    def get(self, request, pk):
+        self.student = get_object_or_404(models.Student, pk=pk)
+        return render(
+            request,
+            'ww/student.html',
+            {'student': self.student}
+        )
 
 
-def student_create(request):
-    if request.method == 'POST':
-        student = models.Student.objects.create(
+class StudentCreateView(View):
+    klasses: tuple
+    student: models.Student
+
+    def get(self, request):
+        self.klasses = models.Klass.objects.all()
+        return render(
+            request,
+            'ww/student_create.html',
+            {'klasses': self.klasses}
+        )
+
+    def post(self, request):
+        self.student = models.Student.objects.create(
             first_name=request.POST['first_name'],
             last_name=request.POST['last_name'],
             age=request.POST['age'],
@@ -30,40 +48,41 @@ def student_create(request):
             photo=request.FILES['photo'],
             klass=get_object_or_404(models.Klass, pk=request.POST['klass'])
         )
-        student.save()
+        self.student.save()
         return redirect('/')
 
-    klasses = models.Klass.objects.all()
-    return render(
-        request,
-        'ww/student_create.html',
-        {'klasses': klasses}
-    )
 
+class StudentUpdateView(View):
+    klasses: tuple
+    student: models.Student
 
-def student_update(request, pk):
-    student = get_object_or_404(models.Student, pk=pk)
-    klasses = models.Klass.objects.all()
-    if request.method == 'POST':
-        student.first_name = request.POST['first_name']
-        student.last_name = request.POST['last_name']
-        student.age = request.POST['age']
-        student.description = request.POST['description']
-        student.photo = request.FILES['photo']
-        student.klass = get_object_or_404(models.Klass, pk=request.POST['klass'])
-        student.save()
+    def get(self, request, pk):
+        self.student = get_object_or_404(models.Student, pk=pk)
+        self.klasses = models.Klass.objects.all()
+        return render(
+            request,
+            'ww/student_update.html',
+            {'klasses': self.klasses, 'student': self.student}
+        )
+
+    def post(self, request, pk):
+        self.student = get_object_or_404(models.Student, pk=pk)
+        self.student.first_name = request.POST['first_name']
+        self.student.last_name = request.POST['last_name']
+        self.student.age = request.POST['age']
+        self.student.description = request.POST['description']
+        self.student.photo = request.FILES['photo']
+        self.student.klass = get_object_or_404(models.Klass, pk=request.POST['klass'])
+        self.student.save()
         return redirect('/')
 
-    return render(
-        request,
-        'ww/student_update.html',
-        {'klasses': klasses, 'student': student}
-    )
 
+class StudentDeleteView(View):
+    student: models.Student
 
-def student_delete(request, pk):
-    student = get_object_or_404(models.Student, pk=pk)
-    student.photo.delete()
-    student.delete()
-    return redirect('/')
+    def get(self, request, pk):
+        self.student = get_object_or_404(models.Student, pk=pk)
+        self.student.photo.delete()
+        self.student.delete()
+        return redirect('/')
 
